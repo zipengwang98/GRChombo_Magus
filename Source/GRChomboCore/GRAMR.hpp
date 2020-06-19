14f1fc0b6f707ccd07f9f629517fb1c3d4323e45
dinatraykova <dina.traykova@physics.ox.ac.uk>
2020-06-19 17:11:43 +0100

    add GRAMR.cpp from Katy's repo with extra functs

--- Source/GRChomboCore/GRAMR.hpp
+++ Source/GRChomboCore/GRAMR.hpp
@@ -20,29 +20,53 @@
  */
 class GRAMR : public AMR
 {
-  private:
-    using Clock = std::chrono::steady_clock;
-    using Hours = std::chrono::duration<double, std::ratio<3600, 1>>;
-    std::chrono::time_point<Clock> start_time = Clock::now();
+private:
+  using Clock = std::chrono::steady_clock;
+  using Hours = std::chrono::duration<double, std::ratio<3600, 1>>;
+  std::chrono::time_point<Clock> start_time = Clock::now();
 
-  public:
-    AMRInterpolator<Lagrange<4>> *m_interpolator; //!< The interpolator pointer
+  // This is used by computeSum, computeNorm, etc.
+  Vector<LevelData<FArrayBox> *> getLevelDataPtrs();
 
-    GRAMR() { m_interpolator = nullptr; } // constructor
+public:
+  AMRInterpolator<Lagrange<4>> *m_interpolator; //!< The interpolator pointer
 
-    auto get_walltime()
-    {
-        auto now = Clock::now();
-        auto duration = std::chrono::duration_cast<Hours>(now - start_time);
+  GRAMR() // constructor
+  {
+    m_interpolator = nullptr;
+  }
 
-        return duration.count();
-    }
+  auto get_walltime()
+  {
+    auto now = Clock::now();
+    auto duration = std::chrono::duration_cast<Hours>(now - start_time);
 
-    // Called after AMR object set up
-    void set_interpolator(AMRInterpolator<Lagrange<4>> *a_interpolator)
-    {
-        m_interpolator = a_interpolator;
-    }
+    return duration.count();
+  }
+
+  // Called after AMR object set up
+  void set_interpolator(AMRInterpolator<Lagrange<4>> *a_interpolator)
+  {
+    m_interpolator = a_interpolator;
+  }
+
+  // Returns the volume-weighted sum of a grid variable
+  Real compute_sum(const int a_comp, const Real a_dx_coarse);
+
+  // Returns the volume-weighted p-norm of an interval of grid variables
+  Real compute_norm(const Interval a_comps, const double a_p,
+		    const Real a_dx_coarse);
+
+  // Returns the max value of an interval of grid variables
+  Real compute_max(const Interval a_comps);
+
+  // Returns the min value of an interval of grid variables
+  Real compute_min(const Interval a_comps);
+
+  // Returns the Infinity norm of an interval of grid variables
+  // This function is a bit pointless because a_p = 0 in compute_norm does the
+  // same
+  Real compute_inf_norm(const Interval a_comps);
 };
 
 #endif /* GRAMR_HPP_ */
