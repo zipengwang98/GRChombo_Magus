98aaee96efd66777908c85d39e592a1db7f33632
dinatraykova <dina.traykova@physics.ox.ac.uk>
2020-07-05 18:19:33 +0100

    Sum over r - not working

--- Examples/FixedBGComplexScalarField/StressExtraction.hpp
+++ Examples/FixedBGComplexScalarField/StressExtraction.hpp
@@ -12,7 +12,7 @@
 #include "SimulationParameters.hpp"
 #include "SmallDataIO.hpp" // for writing data
 #include "SphericalHarmonics.hpp"
-#include "UserVariables.hpp" // Needs c_Stress and c_dArea
+#include "UserVariables.hpp" // Needs c_Stress and c_Momx
 //!  The class allows extraction of the values of the Stress scalar components on
 //!  spherical shells at specified radii, and integration over those shells
 /*!
@@ -26,7 +26,7 @@ class StressExtraction
     //! Params for extraction
     const extraction_params_t m_params;
     const int m_Stress = c_Stress;
-    const int m_dArea = c_dArea;
+    const int m_Momx = c_Momx;
   //const int m_rho = c_rho;
     const double m_dt;
     const double m_time;
@@ -35,24 +35,26 @@ class StressExtraction
     const int m_num_points; // number of points per extraction radius
     const double m_dphi;
     const double m_dtheta;
+    const double m_dx;
 
   public:
     //! The constructor
-    StressExtraction(extraction_params_t a_params, double a_dt, double a_time,
+  StressExtraction(extraction_params_t a_params, double a_dx, double a_dt, double a_time,
                    bool a_first_step, double a_restart_time = 0.0)
         : m_params(a_params), m_dt(a_dt), m_time(a_time),
           m_first_step(a_first_step), m_restart_time(a_restart_time),
           m_num_points(m_params.num_points_phi * m_params.num_points_theta),
           m_dphi(M_PI / m_params.num_points_phi),
-          m_dtheta(M_PI /2.0 / m_params.num_points_theta)
+          m_dtheta(M_PI /2.0 / m_params.num_points_theta),
+	  m_dx(a_dx)
     {
     }
 
     //! The old constructor which assumes it is called in specificPostTimeStep
     //! so the first time step is when m_time == m_dt
-    StressExtraction(extraction_params_t a_params, double a_dt, double a_time,
+  StressExtraction(extraction_params_t a_params, double a_dx, double a_dt, double a_time,
                    double a_restart_time = 0.0)
-        : StressExtraction(a_params, a_dt, a_time, (a_dt == a_time),
+      : StressExtraction(a_params, a_dx, a_dt, a_time, (a_dt == a_time),
                          a_restart_time)
     {
     }
@@ -70,18 +72,18 @@ class StressExtraction
     std::pair<std::vector<double>, std::vector<double>>
     integrate_surface(int es, int el, int em,
                       const std::vector<double> a_Stress,
-                      const std::vector<double> a_dArea) const;
+                      const std::vector<double> a_Momx) const;
 
     //! Write out calculated values of integral for each extraction radius
-    void write_integral(const std::vector<double> a_integral_Stress_1,
-                        const std::vector<double> a_integral_Stress_2,
+    void write_integral(const std::vector<double> a_integral_Stress,
+                        const std::vector<double> a_integral_Momx,
                         std::string a_filename) const;
 
     //! Write out the result of the extraction in phi and theta at each timestep
     //! for each extraction radius
     void write_extraction(std::string a_file_prefix,
                           const std::vector<double> a_Stress,
-                          const std::vector<double> a_dArea) const;
+                          const std::vector<double> a_Momx) const;
 };
 
 #include "StressExtraction.impl.hpp"
--- Examples/FixedBGComplexScalarField/StressExtraction.impl.hpp
+++ Examples/FixedBGComplexScalarField/StressExtraction.impl.hpp
@@ -22,7 +22,7 @@ inline void StressExtraction::execute_query(
 
     std::vector<double> interp_Stress(m_num_points *
                                        m_params.num_extraction_radii);
-    std::vector<double> interp_dArea(m_num_points *
+    std::vector<double> interp_Momx(m_num_points *
                                        m_params.num_extraction_radii);
     std::vector<double> interp_x(m_num_points * m_params.num_extraction_radii);
     std::vector<double> interp_y(m_num_points * m_params.num_extraction_radii);
@@ -55,7 +55,7 @@ inline void StressExtraction::execute_query(
         .setCoords(1, interp_y.data())
         .setCoords(2, interp_z.data())
         .addComp(m_Stress, interp_Stress.data())
-        .addComp(m_dArea, interp_dArea.data());
+        .addComp(m_Momx, interp_Momx.data());
 
     // submit the query
     a_interpolator->interp(query);
@@ -64,7 +64,7 @@ inline void StressExtraction::execute_query(
     {
         const std::pair<int, int> &mode = m_params.modes[imode];
         auto integral = integrate_surface(-2, mode.first, mode.second,
-                                          interp_Stress, interp_dArea);
+                                          interp_Stress, interp_Momx);
         std::string integral_filename = "Stress_integral_" +
                                         std::to_string(mode.first) +
                                         std::to_string(mode.second);
@@ -73,7 +73,7 @@ inline void StressExtraction::execute_query(
 
     if (m_params.write_extraction)
     {
-        write_extraction("ExtractionOut_", interp_Stress, interp_dArea);
+        write_extraction("ExtractionOut_", interp_Stress, interp_Momx);
     }
 }
 
@@ -82,7 +82,7 @@ inline void StressExtraction::execute_query(
 inline std::pair<std::vector<double>, std::vector<double>>
 StressExtraction::integrate_surface(int es, int el, int em,
                                   const std::vector<double> a_Stress,
-				  const std::vector<double> a_dArea) const
+				  const std::vector<double> a_Momx) const
 {
     CH_TIME("StressExtraction::integrate_surface");
     int rank;
@@ -91,9 +91,10 @@ StressExtraction::integrate_surface(int es, int el, int em,
 #else
     rank = 0;
 #endif
-    std::vector<double> integral_Stress_1(m_params.num_extraction_radii, 0.);
-    std::vector<double> integral_Stress_2(m_params.num_extraction_radii, 0.);
-
+    std::vector<double> integral_Stress(m_params.num_extraction_radii, 0.);
+    //std::vector<double> inner_integral_Momx(m_params.num_extraction_radii, 0.);
+    std::vector<double> integral_Momx(m_params.num_extraction_radii, 0.);
+    double inner_integral_Momx = 0.;  
     // only rank 0 does the integral, but use OMP threads if available
     if (rank == 0)
     {
@@ -105,73 +106,64 @@ StressExtraction::integrate_surface(int es, int el, int em,
         // of vector) is equal to the first point
 #ifdef _OPENMP
 #if __GNUC__ > 8
-#define OPENMP_CONST_SHARED shared(a_Stress, a_dArea)
+#define OPENMP_CONST_SHARED shared(a_Stress, a_Momx)
 #else
 #define OPENMP_CONST_SHARED
 #endif
 #pragma omp parallel for collapse(2) default(none)                             \
-    shared(es, el, em, integral_Stress_1, integral_Stress_2) OPENMP_CONST_SHARED
+  shared(es, el, em, integral_Stress, integral_Momx) OPENMP_CONST_SHARED
 #undef OPENMP_CONST_SHARED
 #endif
-        for (int iradius = 0; iradius < m_params.num_extraction_radii;
+      for (int iradius = 0; iradius < m_params.num_extraction_radii;
              ++iradius)
-	  //using namespace SphericalHarmonics;
-        {
-	  double inner_integral_Momx = 0.;
+	{
+	  //	  double inner_integral_Momx = 0.;
 	  for (int iphi = 0; iphi < m_params.num_points_phi; ++iphi)
             {
+	      double inner_integral_Momx = 0.;
 	      double phi = (iphi + 0.5) * m_dphi;
-                double inner_integral_Stress = 0.;
-		double f_theta_phi_r_Momx = 0.;
-                for (int itheta = 0; itheta < m_params.num_points_theta;
-                     itheta++)
+	      double inner_integral_Stress = 0.;
+	      double f_theta_phi_r_Momx = 0.;
+	      for (int itheta = 0; itheta < m_params.num_points_theta;
+		   itheta++)
                 {
 		  using namespace SphericalHarmonics;
-                    double theta = (itheta + 0.5) * m_dtheta;
-                    int idx = iradius * m_num_points +
-                              itheta * m_params.num_points_phi + iphi;
-                    double x = m_params.extraction_radii[iradius] * sin(theta) *
-                               cos(phi);
-                    double y = m_params.extraction_radii[iradius] * sin(theta) *
-                               sin(phi);
-                    double z = m_params.extraction_radii[iradius] * cos(theta);
-	
-                    double integrand_Stress = a_Stress[idx];
-		    double integrand_Momx = a_dArea[idx];
-		    double r2sintheta =  m_params.extraction_radii[iradius] *  m_params.extraction_radii[iradius] * sin(theta);
-                    double f_theta_phi_Stress = integrand_Stress * r2sintheta;
-		    double f_theta_phi_Momx = integrand_Momx * r2sintheta;
-                    inner_integral_Stress += m_dtheta * f_theta_phi_Stress;
-		    f_theta_phi_r_Momx += m_dtheta * f_theta_phi_Momx;
+		  double theta = (itheta + 0.5) * m_dtheta;
+		  int idx = iradius * m_num_points +
+		    itheta * m_params.num_points_phi + iphi;
+		  double x = m_params.extraction_radii[iradius] * sin(theta) *
+		    cos(phi);
+		  double y = m_params.extraction_radii[iradius] * sin(theta) *
+		    sin(phi);
+		  double z = m_params.extraction_radii[iradius] * cos(theta);
+		  
+		  double integrand_Stress = a_Stress[idx];
+		  double integrand_Momx = a_Momx[idx];
+		  double r2sintheta =  m_params.extraction_radii[iradius] *  m_params.extraction_radii[iradius] * sin(theta);
+		  double f_theta_phi_Stress = integrand_Stress * r2sintheta;
+		  double f_theta_phi_Momx = integrand_Momx * r2sintheta;
+		  inner_integral_Stress += m_dtheta * f_theta_phi_Stress;
+		  f_theta_phi_r_Momx += m_dtheta * f_theta_phi_Momx;
                 }
-
-		//if (iphi < m_params.num_points_phi/2)
-		//  {
-		    //pout()<< "first-half" <<iphi<<m_dphi<<endl;
 #ifdef _OPENMP
 #pragma omp atomic
 #endif
-	        integral_Stress_1[iradius] += m_dphi * inner_integral_Stress;
-		inner_integral_Momx += m_dphi * f_theta_phi_r_Momx;
-		    //   }
-		    //else
-		    //{
-		     //pout()<< "second-half" <<iphi<<m_dphi<<endl;
+	      integral_Stress[iradius] += m_dphi * inner_integral_Stress;
+	      inner_integral_Momx += m_dphi * f_theta_phi_r_Momx;
+#ifdef _OPENMP
+#pragma omp atomic
+#endif
+	      integral_Momx[iradius] += m_dx * inner_integral_Momx;
 	    }
-#ifdef _OPENMP
-#pragma omp atomic
-#endif
-	  integral_Stress_2[iradius] += m_dphi * inner_integral_Momx;
-		    //}
         }
     }
-    return std::make_pair(integral_Stress_1, integral_Stress_2);
+    return std::make_pair(integral_Stress, integral_Momx);
 }
 
 //! Write out calculated value of integral for each extraction radius
 inline void
-StressExtraction::write_integral(const std::vector<double> a_integral_Stress_1,
-                               const std::vector<double> a_integral_Stress_2,
+StressExtraction::write_integral(const std::vector<double> a_integral_Stress,
+                               const std::vector<double> a_integral_Momx,
                                std::string a_filename) const
 {
     CH_TIME("StressExtraction::write_integral");
@@ -196,7 +188,7 @@ StressExtraction::write_integral(const std::vector<double> a_integral_Stress_1,
             int iintegral1 = iintegral + 1;
             int iradius = iintegral / 2;
             header1_strings[iintegral] = "integral Stress";
-            header1_strings[iintegral1] = "integral Stress";
+            header1_strings[iintegral1] = "integral Mom_x";
             header2_strings[iintegral1] = header2_strings[iintegral] =
                 std::to_string(m_params.extraction_radii[iradius]);
         }
@@ -213,8 +205,8 @@ StressExtraction::write_integral(const std::vector<double> a_integral_Stress_1,
     {
         int iintegral1 = iintegral + 1;
         int iradius = iintegral / 2;
-        data_for_writing[iintegral] = a_integral_Stress_1[iradius];
-        data_for_writing[iintegral1] = a_integral_Stress_2[iradius];
+        data_for_writing[iintegral] = a_integral_Stress[iradius];
+        data_for_writing[iintegral1] = a_integral_Momx[iradius];
     }
 
     // write data
@@ -226,7 +218,7 @@ StressExtraction::write_integral(const std::vector<double> a_integral_Stress_1,
 inline void
 StressExtraction::write_extraction(std::string a_file_prefix,
                                    const std::vector<double> a_Stress,
-				   const std::vector<double> a_dArea) const
+				   const std::vector<double> a_Momx) const
 {
     CH_TIME("StressExtraction::write_extraction");
     SmallDataIO extraction_file(a_file_prefix, m_dt, m_time, m_restart_time,
@@ -241,7 +233,7 @@ StressExtraction::write_extraction(std::string a_file_prefix,
         extraction_file.write_header_line(header1_strings, "");
         std::vector<std::string> components = {
             UserVariables::variable_names[m_Stress],
-            UserVariables::variable_names[m_dArea]};
+            UserVariables::variable_names[m_Momx]};
         std::vector<std::string> coords = {"theta", "phi"};
         extraction_file.write_header_line(components, coords);
 
@@ -256,7 +248,7 @@ StressExtraction::write_extraction(std::string a_file_prefix,
             double theta = (itheta + 0.5) * m_dtheta;
             double phi = (iphi + 0.5) * m_dphi;
 
-            extraction_file.write_data_line({a_Stress[idx], a_dArea[idx]},
+            extraction_file.write_data_line({a_Stress[idx], a_Momx[idx]},
                                             {theta, phi});
         }
         extraction_file.line_break();
